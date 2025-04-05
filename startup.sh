8f426553dec221a43ddcf2ffdd290504358ecead
div5252 <divyamsingal01@gmail.com>
2025-04-05 10:42:21 +0530

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000
+uvicorn app:app --reload
