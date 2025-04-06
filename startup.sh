1759b4efb3a5c11b4d54f3e65b4bc28db1172c92
div5252 <divyamsingal01@gmail.com>
2025-04-06 11:26:12 +0530

    Update workflow

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+apt-get install protobuf-compiler libprotoc-dev
 
 # uvicorn app:app --reload
 gunicorn -k uvicorn.workers.UvicornWorker app:app
